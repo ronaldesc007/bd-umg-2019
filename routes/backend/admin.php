10806a783f627fa3525bc77d84b348acf9eb907a
Donald <donald.santizo@gmail.com>
2019-09-12 17:47:33 -0600

    Update admin.php

--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -11,9 +11,9 @@ use App\Http\Controllers\ControllerActor;
 // All route names are prefixed with 'admin.'.
 Route::redirect('/', '/admin/dashboard', 301);
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
 Route::get('peliculas', [ControllerPelicula::class, 'index'])->name('peliculas');
 Route::get('actor', [ControllerActor::class, 'index'])->name('actor');
+
 Route::post('peliculas/guardar', [ControllerPelicula::class, 'store'])->name('peliculas.guardar');
 Route::get('peliculas/crear', [ControllerPelicula::class, 'create'])->name('peliculas.crear');
 
@@ -26,6 +26,5 @@ Route::get('peliculas/{cod_pelicula}/editar', [ControllerPelicula::class, 'edit'
 
 Route::get('sincronizacion', [ControllerSincro::class, 'index'])->name('sincronizacion');
 
-Route::post('actor/guardar', [ControllerPelicula::class, 'store'])->name('actor.guardar');
-Route::get('actor/crear', [ControllerPelicula::class, 'create'])->name('actor.crear');
+
 
