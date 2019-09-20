4e10fc49a5be3dee9edcc4dd6f71abd6fe78399e
Donald <donald.santizo@gmail.com>
2019-09-20 09:21:57 -0600

    Update ModelActor.php

--- app/ModelActor.php
+++ app/ModelActor.php
@@ -4,9 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class ModelActor extends Model
 {
     protected $table = 'actor';
     protected $primaryKey = 'cod_actor';
     
 }
+
