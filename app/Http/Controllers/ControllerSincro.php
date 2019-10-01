9b626587e2264b9ff500002d040061de2ebeadf0
Donald <donald.santizo@gmail.com>
2019-09-30 23:23:28 -0600

    sync_actores

--- app/Http/Controllers/ControllerSincro.php
+++ app/Http/Controllers/ControllerSincro.php
@@ -30,6 +30,7 @@ class ControllerSincro extends Controller
         DB::beginTransaction();
         
         $this->syncPeliculas($motorbd,$motorbd2); 
+        $this->syncActore($motorbd,$motorbd2); 
         
         DB::commit(); // Commit if no error
         
@@ -265,7 +266,232 @@ class ControllerSincro extends Controller
         return true;
         
     }
+    
+    /**
+     * Actualizar los actores.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    private function syncActores($motorbd,$motorbd2)
+    {
         
+        Log::alert('INICIANDO PROCESO DE SINCRONIZACION DE '.$motorbd.' A '.$motorbd2);
+        
+        $actores_bd1 = DB::connection($motorbd)->table('actor')->where('isDeleted','<>',1)->get();
+        Log::alert('Actores Activos en BD1 '.$motorbd.': '.$actores_bd1->count());
+        
+        $actores_bd2 = DB::connection($motorbd2)->table('actor')->where('isDeleted','<>',1)->get();
+        Log::alert('Actores Activos en BD2 '.$motorbd2.': '.$actores_bd2->count());
+        
+        // Actores Nuevos
+        $actores_nuevos = ModelActor::where('isSynced','=',0)->where('isDeleted','=',0)->where('isUpdated','=',0)->get('cod_actor');
+        Log::alert('Sincronizando Actores Nuevos: '.$actores_nuevos->count());
+        
+        foreach($actores_nuevos as $anuevo) {
+            
+            if ($actores_bd2->contains('cod_actor', '=', $anuevo->cod_actor)) {
+                
+                Log::alert('El siguiente codigo de actor ya existe en BD2 : '.$anuevo->cod_actor .'');
+                
+                // buscando modelo en bd2
+                $actorbd2 = ModelActor::on($motorbd2)->find($anuevo->cod_actor);
+                
+                // buscando modelo en bd1
+                $actorbd1 = ModelActor::find($anuevo->cod_actor);
+                
+                // comparando modelo bd1 vs modelo bd2
+                if ($actorbd2->created_at >= $actorbd1->created_at ) {
+                        
+                    Log::alert('El registo en BD1 es mas antiguo, actualizando BD2 : '.$anuevo->cod_actor .'');
+                    
+                    // el registro en bd1 es mas antiguo, actualizando el registro en bd2 con los datos de bd1
+                    $actorbd2->nombre = $actorbd1->nombre;
+                    $actorbd2->fecha_nacimiento = $actorbd1->fecha_nacimiento;
+                    
+                    //marcando ambos modelos como sincronizados
+                    $actorbd1->isSynced = 1;
+                    $actorbd2->isSynced = 1;
+
+                    // guardando los cambios
+                    $actorbd1->save();
+                    $actorbd2->save();
+                    
+                    if (!$actorbd1) {
+                        DB::rollback(); //Rollback Transaction
+                        return Redirect::back()->withInput()->withFlashDanger('DB::Error');
+                    }
+                    
+                    if (!$actorbd2) {
+                        DB::rollback(); //Rollback Transaction
+                        return Redirect::back()->withInput()->withFlashDanger('DB::Error');
+                    }
+                    
+                }
+                else {
+                    
+                    Log::alert('El registo en BD2 es mas antiguo, actualizando BD1 : '.$anuevo->cod_actor .'');
+                    // el registro en bd2 es mas antiguo, actualizando el registro en bd2 con los datos de bd1
+                    $actorbd1->nombre = $actorbd2->nombre;
+                    $actorbd1->fecha_nacimiento = $actorbd2->fecha_nacimiento;
+                    
+                    //marcando ambos modelos como sincronizados
+                    $actorbd1->isSynced = 1;
+                    $actorbd2->isSynced = 1;
+
+                    // guardando los cambios
+                    $actorbd1->save();
+                    $actorbd2->save();
+                    
+                    if (!$actorbd1) {
+                        DB::rollback(); //Rollback Transaction
+                        return Redirect::back()->withInput()->withFlashDanger('DB::Error');
+                    }
+                    
+                    if (!$actorbd2) {
+                        DB::rollback(); //Rollback Transaction
+                        return Redirect::back()->withInput()->withFlashDanger('DB::Error');
+                    }
+                    
+                }
+                    
+            } else {
+                Log::alert('La pelicula no existe, se agrega : '.$anuevo->cod_actor);
+                
+                // buscando la el modelo existente
+                $actorbd1 = ModelPelicula::find($anuevo->cod_actor);
+                // creando una copia del modelo existente
+                $actorbd2 = $actorbd1->replicate();
+                // asignando el ID del modelo en bd2
+                $actorbd2->cod_actor = $anuevo->cod_actor;
+                // cambiando la conexion de bd
+                $actorbd2->setConnection($motorbd2);
+                
+                //marcando ambos modelos como sincronizados
+                $actorbd1->isSynced = 1;
+                $actorbd2->isSynced = 1;
+                
+                $actorbd1->save();
+                $actorbd2->save();
+                
+                if (!$actorbd2) {
+                    DB::rollback(); //Rollback Transaction
+                    return Redirect::back()->withInput()->withFlashDanger('DB::Error');
+                }
+                
+            }           
+            
+        } 
+        
+        // actualizando secuencia de Postgresql
+        if($motorbd=='mysql') {
+            $max = DB::table('actor')->max('cod_actor') + 1; 
+            DB::connection($motorbd2)->statement("ALTER SEQUENCE actor_cod_actor_seq RESTART WITH $max");
+        } 
+                
+        
+        // Actores Actualizados
+        $actores_editados = ModelActor::where('isUpdated','=',1)->where('isSynced','=',0)->where('isDeleted','=',0)->get('cod_actor');
+        Log::alert('Sincronizando Actores Editados: '.$actores_editados->count());
+        
+        foreach($actores_editados as $aeditado) {
+            
+            if ($actorbd2->contains('cod_actor', '=', $aeditado->cod_actor)) {
+                
+                Log::alert('El siguiente codigo de actor ya existe en BD2 : '.$aeditado->cod_actor .'');
+                
+                // buscando modelo en bd2
+                $actorbd2 = ModelActor::on($motorbd2)->find($aeditado->cod_actor);
+                
+                // buscando modelo en bd1
+                $actorbd1 = ModelActor::find($aeditado->cod_actor);
+                
+                // comparando modelo bd1 vs modelo bd2
+                if ($actorbd2->updated_at <= $actorbd1->updated_at ) {
+                        
+                    Log::alert('El registo en BD1 es mas reciente, actualizando BD2 : '.$aeditado->cod_actor .'');
+                    
+                    // el registro en bd1 es mas reciente, actualizando el registro en bd2 con los datos de bd1
+                    $actorbd2->titulo = $actorbd1->titulo;
+                    $actorbd2->categoria = $actorbd1->categoria;
+                    
+                    //marcando ambos modelos como sincronizados
+                    $actorbd1->isUpdated = 0;
+                    $actorbd1->isSynced = 1;
+                    $actorbd2->isUpdated = 0;
+                    $actorbd2->isSynced = 1;
+                    
+
+                    // guardando los cambios
+                    $actorbd1->save();
+                    $actorbd2->save();
+                    
+                    if (!$actorbd1) {
+                        DB::rollback(); //Rollback Transaction
+                        return Redirect::back()->withInput()->withFlashDanger('DB::Error');
+                    }
+                    
+                    if (!$actorbd2) {
+                        DB::rollback(); //Rollback Transaction
+                        return Redirect::back()->withInput()->withFlashDanger('DB::Error');
+                    }
+                    
+                }
+                else {
+                    
+                    Log::alert('El registo en BD2 es mas reciente, actualizando BD1 : '.$anueva->cod_actor .'');
+                    // el registro en bd2 es mas reciente, actualizando el registro en bd2 con los datos de bd1
+                    $actorbd1->nombre = $actorbd2->titulo;
+                    $actorbd1->fecha_nacimiento = $actorbd2->fecha_nacimiento;
+                    
+                    //marcando ambos modelos como sincronizados
+                    $actorbd1->isUpdated = 0;
+                    $actorbd1->isSynced = 1;
+                    $actorbd2->isUpdated = 0;
+                    $actorbd2->isSynced = 1;
+                    
+                    // guardando los cambios
+                    $actorbd1->save();
+                    $actorbd2->save();
+                    
+                    if (!$actorbd1) {
+                        DB::rollback(); //Rollback Transaction
+                        return Redirect::back()->withInput()->withFlashDanger('DB::Error');
+                    }
+                    
+                    if (!$actorbd2) {
+                        DB::rollback(); //Rollback Transaction
+                        return Redirect::back()->withInput()->withFlashDanger('DB::Error');
+                    }
+                    
+                }
+                    
+            }
+            
+            
+        } 
+        
+        // Peliculas Eliminadas
+        $actores_eliminados = ModelActor::where('isDeleted','=',1)->get();
+        Log::alert('Sincronizando Actores Eliminados: '.$actores_eliminados->count());
+        
+        foreach($actores_eliminados as $adel) {
+            
+            if ($actorbd2->contains('cod_actor', '=', $adel->cod_actor)) {
+                Log::alert('Borrando Actor de ambas BD: '.$adel->cod_actor);
+                DB::connection($motorbd2)->table('actor')->where('cod_actor','=',$adel->cod_actor)->delete(); 
+                DB::connection($motorbd)->table('actor')->where('cod_actor','=',$adel->cod_actor)->delete(); 
+            } else {
+                Log::alert('Borrando solo de BD1: '.$adel->cod_actor);
+                DB::connection($motorbd)->table('actor')->where('cod_actor','=',$adel->cod_actor)->delete(); 
+            }        
+            
+        }  
+        
+        Log::alert('Sincronizacion de Actores Completa: '.$actores_eliminados->count());
+        
+        return true;
+        
+    }
     /**
      * Show the form for creating a new resource.
      *
