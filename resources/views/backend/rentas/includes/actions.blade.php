ccd80c066467762864f9ac6ecd1a0e02a77b1c61
Ronald Santizo <ronaldesc007@gmail.com>
2019-10-01 17:26:30 -0600

    Quitando boton Visualizar

--- resources/views/backend/rentas/includes/actions.blade.php
+++ resources/views/backend/rentas/includes/actions.blade.php
@@ -1,9 +1,6 @@
 
     <div class="btn-group" role="group" aria-label="@lang('labels.backend.access.users.user_actions')">
-        <a href="{{ route('admin.rentas.ver', $cod_renta) }}" data-toggle="tooltip" data-placement="top" title="@lang('buttons.general.crud.view')" class="btn btn-info">
-            <i class="fas fa-eye"></i>
-        </a>
-
+        
         <a href="{{ route('admin.rentas.editar', $cod_renta) }}" data-toggle="tooltip" data-placement="top" title="@lang('buttons.general.crud.edit')" class="btn btn-primary">
             <i class="fas fa-edit"></i>
         </a>
